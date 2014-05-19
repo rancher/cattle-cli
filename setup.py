b823001b10174f109e18db0308cec7aa3d461a14
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-05-19 13:35:19 -0700

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./requirements.txt') as r:
 
 setup(
     name='cattle',
-    version='0.2.2',
+    version='0.3.0',
     py_modules=['cattle'],
     url='https://github.com/cattleio/cattle-cli',
     license='MIT Style',
